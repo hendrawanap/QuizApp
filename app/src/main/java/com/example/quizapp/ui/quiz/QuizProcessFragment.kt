7c7578b65c84ba1fd518a3e773dfd5df4e54a46a
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2021-11-29 12:14:04 +0700

    Add trim function on short answer (3)

--- app/src/main/java/com/example/quizapp/ui/quiz/QuizProcessFragment.kt
+++ app/src/main/java/com/example/quizapp/ui/quiz/QuizProcessFragment.kt
@@ -148,7 +148,7 @@ class QuizProcessFragment : Fragment() {
 
                 binding.viewShortAnswer.apply {
                     when (viewModel.currentAnswer.value) {
-                        answerField.text.toString() -> changeStyle("correct", answerField)
+                        answerField.text.toString().trim() -> changeStyle("correct", answerField)
                     }
                 }
             } else {
